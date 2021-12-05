88ec3ba33200b483ee43845fbb482d8592a08650
joeface71 <joefacenda@yahoo.com>
2021-12-05 07:28:15 -0500

    Finished weapon rendering

--- Assets/_Scripts/AgentWeapon.cs
+++ Assets/_Scripts/AgentWeapon.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,25 @@ public class AgentWeapon : MonoBehaviour
 {
     protected float desiredAngle;
 
+    [SerializeField]
+    protected WeaponRenderer weaponRenderer;
+
+    private void Awake()
+    {
+        weaponRenderer = GetComponentInChildren<WeaponRenderer>();
+    }
+
     public virtual void AimWeapon(Vector2 pointerPosition)
     {
         var aimDirection = (Vector3)pointerPosition - transform.position;
         desiredAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        AdjustWeaponRendering();
         transform.rotation = Quaternion.AngleAxis(desiredAngle, Vector3.forward);
+    }
+
+    protected void AdjustWeaponRendering()
+    {
+        weaponRenderer?.FlipSprite(desiredAngle > 90 || desiredAngle < -90);
+        weaponRenderer?.RenderBehindHead(desiredAngle < 180 && desiredAngle > 0);
     }
 }
